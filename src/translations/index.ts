724d39be74f35ab9567a1278f0c2cba238a75f42
Raul <raulvalesc@gmail.com>
2023-12-30 19:24:48 +0100

    Format all files.

--- src/translations/index.ts
+++ src/translations/index.ts
@@ -1,15 +1,19 @@
-import english from './en.json';
-import spanish from './es.json';
+import english from "./en.json";
+import spanish from "./es.json";
 
+// Declare the available languages on the application
 const AVAILABLE_LANGUAGES = {
-    ENGLISH: 'en',
-    SPANISH: 'es',
-}
+    ENGLISH: "en",
+    SPANISH: "es",
+};
 
 export const getTranslations = (currentLocale?: string): any => {
+    // Check if the current locale is english
     if (currentLocale == AVAILABLE_LANGUAGES.ENGLISH) {
+        // Return english language
         return english;
     }
 
+    // By default, return spanish language
     return spanish;
-}
+};
